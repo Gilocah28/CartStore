976f5870a5357bf3a13c01e3da859127075cd182
Gilocah28 <gberxx@gmail.com>
2024-11-23 20:50:23 +0800

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -54,9 +54,16 @@ const App = () => {
   };
 
   const handleRemoveFromCart = (id: number) => {
-    setCartItems((prev) =>
-      prev.reduce((ack, item) => {}, [] as CartItemType[])
-    );
+    setCartItems((prev) => {
+      return prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[]);
+    });
   };
 
   if (isLoading) return <LinearProgress />;
